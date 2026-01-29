b2bfce7209eac7f1ea5bac64d0f68f640aaceefc
githubscientist <sathish.datascientist@gmail.com>
2026-01-29 21:03:44 +0530

    event handling + dom manipulation

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ async function getMeaning(word) {
     try {
         const response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word);
         const data = await response.json();
-        console.log(data);
+
+        // get the paragraph
+        const meaning = document.querySelector('#meaning');
+
+        meaning.textContent = data[0].meanings[0].definitions[0].definition;
     } catch (error) {
         console.log(error);
     }
@@ -14,6 +18,12 @@ async function getMeaning(word) {
 const btnSubmit = document.querySelector('#btnSubmit');
 
 // attach an event listener
-btnSubmit.addEventListener('click', () => {
-    console.log('button clicked');
+btnSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    // get the reference of the wordInput -> input text box where the user
+    // types the word to get the meaning
+    const wordInput = document.querySelector('#wordInput');
+
+    getMeaning(wordInput.value);
 })
