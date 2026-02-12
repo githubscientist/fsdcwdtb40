370ca23cca591b0e2fec0da28b98106762d8e58b
githubscientist <sathish.datascientist@gmail.com>
2026-02-12 19:33:59 +0530

    http request --using fetch

--- script.js
+++ script.js
@@ -3,37 +3,29 @@ const fetchTodosButton = document.getElementById('fetchTodosButton');
 const todoBlock = document.getElementById('todoBlock');
 
 const fetchTodos = () => {
-    // 1. create an instance of the XMLHttpRequest object
-    const xhr = new XMLHttpRequest();
+    fetch('https://698dd177b79d1c928ed6a345.mockapi.io/todos')
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            // create a header element
+            const header = document.createElement('h1');
 
-    // 2. Open a connection to the API endpoint
-    xhr.open('GET', 'https://698dd177b79d1c928ed6a345.mockapi.io/todos');
+            header.textContent = 'Todos';
 
-    // 3. Set up a callback function to handle the response (onload)
-    xhr.onload = () => {
-        const response = JSON.parse(xhr.responseText);
+            // create a unordered list
+            const ul = document.createElement('ul');
 
-        // create a header element
-        const header = document.createElement('h1');
+            for (let i = 0; i < response.length; i++) {
+                const li = document.createElement('li');
 
-        header.textContent = 'Todos';
+                li.textContent = response[i].title;
 
-        // create a unordered list
-        const ul = document.createElement('ul');
+                ul.appendChild(li);
+            }
 
-        for (let i = 0; i < response.length; i++) {
-            const li = document.createElement('li');
-
-            li.textContent = response[i].title;
-
-            ul.appendChild(li);
-        }
-
-        todoBlock.append(header, ul);
-    }
-
-    // 4. send the request to the server
-    xhr.send();
+            todoBlock.append(header, ul);
+        })
 }
 
 // attach an event listener to the button
